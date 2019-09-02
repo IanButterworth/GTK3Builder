0bd96006a7aae10571012374fd0b464db60f745c
Ian <i.r.butterworth@gmail.com>
2019-09-02 12:57:11 -0400

    Update build_tarballs.jl

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -35,18 +35,32 @@ products(prefix) = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     # The GLib library provides core non-graphical functionality such as high level data types, Unicode manipulation, and an object and type system to C programs.
+    # Glib-related dependencies
+    "https://github.com/JuliaPackaging/Yggdrasil/releases/download/PCRE-v8.42-2/build_PCRE.v8.42.0.jl",
+    "https://github.com/giordano/Yggdrasil/releases/download/Libffi-v3.2.1-0/build_Libffi.v3.2.1.jl",
+    "https://github.com/JuliaPackaging/Yggdrasil/releases/download/Libiconv-v1.15-0/build_Libiconv.v1.15.0.jl",
+    "https://github.com/JuliaPackaging/Yggdrasil/releases/download/Gettext-v0.19.8-0/build_Gettext.v0.19.8.jl",
     "https://github.com/JuliaPackaging/Yggdrasil/releases/download/Glib-v2.59.0%2B0/build_Glib.v2.59.0.jl",
+
     # The GdkPixbuf library provides facilities for loading images in a variety of file formats.
+    # GdkPixBuf dependencies
+    "https://github.com/bicycle1885/ZlibBuilder/releases/download/v1.0.4/build_Zlib.v1.2.11.jl",
+    "https://github.com/giordano/Yggdrasil/releases/download/X11-v1.6.8/build_X11.v1.6.8.jl",
+    "https://github.com/JuliaIO/LibpngBuilder/releases/download/v1.0.3/build_libpng.v1.6.37.jl",
+    "https://github.com/JuliaPackaging/Yggdrasil/releases/download/JpegTurbo-v2.0.1-0/build_JpegTurbo.v2.0.1.jl",
+    "https://github.com/JuliaIO/LibTIFFBuilder/releases/download/v6/build_libtiff.v4.0.9.jl",
+    "https://github.com/ianshmean/SharedMimeInfoBuilder/releases/download/v1.0.0/build_shared-mime-info.v1.10.0.jl",
 
     # Pango is a library for internationalized text handling.
-    "https://github.com/giordano/PangoBuilder/releases/download/v1.42.4/build_Pango.v1.42.4.jl",
+    # Pango-only dependencies
+    "https://github.com/JuliaPackaging/Yggdrasil/releases/download/FriBidi-v1.0.5%2B0/build_FriBidi.v1.0.5.jl",
+    "https://github.com/giordano/Yggdrasil/releases/download/Pango-v.1.42.4/build_Pango.v1.42.4.jl"
 
     # ATK is the Accessibility Toolkit. It provides a set of generic interfaces allowing accessibility technologies such as screen readers to interact with a graphical user interface.
 
+
     # Gobject Introspection is a framework for making introspection data available to language bindings.
     ### May be satisfied via the GLib build...
-
-    "https://github.com/JuliaPackaging/Yggdrasil/releases/download/FriBidi-v1.0.5%2B0/build_FriBidi.v1.0.5.jl"
 
 ]
 
